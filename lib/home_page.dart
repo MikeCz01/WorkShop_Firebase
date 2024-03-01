d844d5a403adc1cb6441d45a59bef3b4f6eee09a
Miguel Alejandro Cruz <miguelcruz5734@gmail.com>
2024-03-01 16:42:41 -0600

    Se agregó el archivo guest_book

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart' // new
-    hide EmailAuthProvider, PhoneAuthProvider;    // new
-import 'package:flutter/material.dart';           // new
-import 'package:provider/provider.dart';          // new
-
-import 'app_state.dart';                          // new
-import 'src/authentication.dart';                 // new
+    hide
+        EmailAuthProvider,
+        PhoneAuthProvider; // new
+import 'package:flutter/material.dart'; // new
+import 'package:provider/provider.dart'; // new
+import 'guest_book.dart';
+import 'app_state.dart'; // new
+import 'src/authentication.dart'; // new
 import 'src/widgets.dart';
 
 class HomePage extends StatelessWidget {
@@ -42,6 +44,8 @@ class HomePage extends StatelessWidget {
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
           ),
+          const Header('Discussion'),
+          GuestBook(addMessage: (message) => print(message)),
         ],
       ),
     );
